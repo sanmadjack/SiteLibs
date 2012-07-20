c70e17062cc3526953702c825bd3e7d3406d20a7
Matthew Barbour <sanmadjack@gmail.com>
2012-07-20 10:03:44 -0500

    I don't remember

--- popups.js
+++ popups.js
@@ -3,13 +3,16 @@ function setUpPopUps() {
     $('a.popup_close').off('click');
     $('a.popup_link').click(function(e) {
         e.preventDefault(); 
+    });
+    $('a.popup_link').mousedown(function(e) {
+        e.preventDefault(); 
         $(".popup").stop(true, true).fadeOut();
 
         var div = $(this).attr('href');
         $("#" + div).stop(true, true).fadeIn();
     });
     $('div.popup').append('<a class="popup_close">Close</a>');
-    $('a.popup_close').click(function(e) {
+    $('a.popup_close').mousedown(function(e) {
         $(this).parent().stop(true, true).fadeOut();
     });
 }
