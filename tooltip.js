c89a33fac04dc8aa802decaf62790de7408b48f8
Matthew Barbour <sanmadjack@gmail.com>
2012-08-17 13:37:37 -0500

    changed tooltip so that it auto-generates the location of the tooltip

--- tooltip.js
+++ tooltip.js
@@ -1,6 +1,24 @@
 function setUpToolTips() {    
     $('.has_tooltip').off('hover');
     $('.has_tooltip').hover(function() {
+        
+        var top = $(this).height();
+        var width = $(this).children(".tooltip").width();
+        var parent_left = $(this).offset().left;
+        
+        var doc_width = $(document).innerWidth() - 50;
+        if(parent_left + width > doc_width) {
+          $(this).children(".tooltip").css({ top: top, right: 15 });
+            
+        } else {
+            
+          $(this).children(".tooltip").css({ top: top, left: 35 });
+            
+        }
+        
+        
+        
+        
       $(this).children(".tooltip").stop(true, true).fadeIn();
     }, function() {
       $(this).children(".tooltip").stop(true, true).fadeOut();
